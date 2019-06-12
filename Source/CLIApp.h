1fced9394d7e4e2be3e7d914ca36526ca08ad8d1
Charles Holbrow <CharlesHolbrow@gmail.com>
2019-06-12 02:29:14 -0400

    --list-io argument

--- Source/CLIApp.h
+++ Source/CLIApp.h
@@ -83,6 +83,9 @@ class CLIApp : public JUCEApplicationBase, ChangeListener {
     void listProjects();
     void listClips();
     void listTracks();
+    void listIoDevices();
+    void listWaveDevices();
+    void listMidiDevices();
     void play();
 
     /** Create and activate an empty edit
