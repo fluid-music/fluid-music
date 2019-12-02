a943905f3c1cc3459345720a583211739de30f7d
Charles Holbrow <CharlesHolbrow@gmail.com>
2019-12-01 19:02:56 -0500

    tab and tab tests

--- examples/basics/src/tab.js
+++ examples/basics/src/tab.js
@@ -1,8 +1,9 @@
-const isEmpty =     (char) => char === ' ' || char === '.';
-const notEmpty =    (char) => !isEmpty(char);
-const isQuarter =   (char) => char.length === 1 && '1234567890'.includes(char);
-const isEighth =    (char) => char === '+';
-const isSixteenth = (char) => char === 'a' || char === 'e';
+const isEmpty =   (char) => char === ' ' || char === '.';
+const notEmpty =  (char) => !isEmpty(char);
+const isQuarter = (char) => char.length === 1 && '1234567890'.includes(char);
+const is8th =     (char) => char === '+';
+const is16th =    (char) => char === 'a' || char === 'e';
+const is32nd =    (char) => char === 't';
 
 const division = (char) => {
   if (typeof char !== 'string' || char.length !== 1)
@@ -10,20 +11,36 @@ const division = (char) => {
 
   if (isEmpty(char)) return 0;
   if (isQuarter(char)) return 1/4;
-  if (isEighth(char)) return 1/8;
-  if (isSixteenth(char)) return 1/16;
+  if (is8th(char)) return 1/8;
+  if (is16th(char)) return 1/16;
+  if (is32nd(char)) return 1/32;
 
   throw new Error(`No division for '${char}' character`);
 }
 
+/**
+ * Convert each character in an string to an duration.
+ * In the following examples, q=1/4 and e=1/8
+ * Quarter notes:
+ *   rhythm - '1+2+'
+ *   result - [e,e,e,e]
+ * Spaces leave a 0 in the array.
+ *   rhythm - '1 + '
+ *   result - [e,0,e,0]
+ * Eight and quarter notes:
+ *   rhythm - '1234+'
+ *   result - [q,q,q,e,e]
+ *
+ * See tests for more examples.
+ * @param {string} rhythm - String representing of a rhythm
+ * @returns {number[]}  - An array of durations for each character
+ */
 const rhythmToAdvanceArray = function(rhythm) {
   if (typeof rhythm === 'string') rhythm = rhythm.split('');
 
   const result = [];
 
   rhythm.forEach((char, i, array) => {
-    if (i+1 === array.length) return;
-    let amount = null; 
     let next = null; // next non-zero value
     for (const c of array.slice(i+1)) {
       if (notEmpty(c)) {
@@ -32,6 +49,7 @@ const rhythmToAdvanceArray = function(rhythm) {
       }
     }
 
+    let amount = null;
     if (isEmpty(char)) 
       amount = 0;
     else if (next === null)
@@ -47,18 +65,10 @@ const rhythmToAdvanceArray = function(rhythm) {
 
 
 const rhythmToElapsedArray = function(rhythm) {
-  if (typeof rhythm === 'string') rhythm = rhythm.split('');
+  rhythm = rhythmToAdvanceArray(rhythm);
 
-  let elapsed = 0;
-  let previous;
-  const result = rhythm.map((char, i, array) => {
-    const prev = array[i-1];
-    if (' .'.includes(char)) return 0;
-    if (typeof previous !== 'string') return;
-    // if (previous )
-
-  });
-  return result;
+  let accumulator = 0;
+  return rhythm.map((value) => accumulator += value);
 };
 
 
--- examples/basics/test/test-tab.js
+++ examples/basics/test/test-tab.js
@@ -1,46 +1,42 @@
 const should = require('should');
 const mocha = require('mocha');
-
 const tab = require('../src/tab');
 
 describe('tab.rhythmToAdvanceArray', () => {
-  const s = 1/16; // sixteenth note
-  const e = 1/8;  // eighth note
   const q = 1/4;  // quarter note
+  const e = 1/8;  // eighth note
+  const s = 1/16; // sixteenth note
 
   const eighthRhythm = '1+2+'; 
-  const eighthAdvances = [e,e,e];
-  it(`should handle ${eighthRhythm}`, () => {
-    tab.rhythmToAdvanceArray(eighthRhythm).should.deepEqual(eighthAdvances);
+  it(`should interpret '${eighthRhythm}' as four eighth notes`, () => {
+    tab.rhythmToAdvanceArray(eighthRhythm).should.deepEqual([e,e,e,e]);
   });
 
   it('should output zeros for spaces', () => {
-    tab.rhythmToAdvanceArray('1 + ') .should.deepEqual([0.125, 0, 0.125]);
-    tab.rhythmToAdvanceArray('1 + 2').should.deepEqual([0.125, 0, 0.125, 0]);
+    tab.rhythmToAdvanceArray('1 + ') .should.deepEqual([e, 0, e, 0]);
+    tab.rhythmToAdvanceArray('1 + 2').should.deepEqual([e, 0, e, 0, q]);
+  });
 
-    // This next one is a bad practice, but it illustrates expected behavior
-    tab.rhythmToAdvanceArray('1 + 2 ').should.deepEqual([e, 0, e, 0, q]);
-    //                                                   1  _  +  _  2
+  const eighthsAndQuarter = '1 + 2 ';
+  it(`should interpret '${eighthsAndQuarter}' as two two eighths and a quarter'`, () => {
+    tab.rhythmToAdvanceArray(eighthsAndQuarter).should.deepEqual([e, 0, e, 0, q, 0]);
   });
 
   const sixteenthsRhythm = '1e+a2e+a';
-  const sixteenthsAdvances = new Array(7).fill(s);
-  it(`should handle ${sixteenthsRhythm}`, () => {
-    tab.rhythmToAdvanceArray(sixteenthsRhythm).should.deepEqual(sixteenthsAdvances);
+  it(`should interpret '${sixteenthsRhythm}' as sixteenths`, () => {
+    tab.rhythmToAdvanceArray(sixteenthsRhythm).should.deepEqual(new Array(8).fill(s));
   });
 
   const mixedRhythm = '1e+a2+'
-  it(`should handle ${mixedRhythm}`, () => {
-
-    tab.rhythmToAdvanceArray(mixedRhythm).should.deepEqual([s,s,s,s,e]);
+  it(`should interpret '${mixedRhythm}' as four sixteenths and two quarters`, () => {
+    tab.rhythmToAdvanceArray(mixedRhythm).should.deepEqual([s,s,s,s,e,e]);
   });
 });
 
 describe('tab.rhythmToElapsedArray', () => {
-  return;
-  let rhythm   = '1e+a2e+a'
-  const desired1 = rhythm.split('').map((letter, i) => i * 1/16);
-  const desired2 = [0, 0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, .4375];
+  let rhythm     = '1e+a2e+a';
+  const desired1 = rhythm.split('').map((letter, i) => (i+1) * 1/16);
+  const desired2 = [0.0625, 0.125, 0.1875, 0.25, 0.3125, 0.375, .4375, 0.5];
 
   it('manual calculations should match generated', () => {
     desired1.should.deepEqual(desired2);
@@ -51,6 +47,6 @@ describe('tab.rhythmToElapsedArray', () => {
   });
 
   it('should handle 1+2+', () => {
-    tab.rhythmToElapsedArray('1+2+').should.deepEqual([0, 0.125, 0.25, 0.375]);
+    tab.rhythmToElapsedArray('1+2+').should.deepEqual([0.125, 0.25, 0.375, 0.5]);
   });
 });
