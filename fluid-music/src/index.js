281967176cfd24767bb0e9abcb8d2e9d43085892
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-01-20 17:06:18 -0500

    simpler fluid exports, so VS Code can find properties

--- fluid-music/src/index.js
+++ fluid-music/src/index.js
@@ -1,10 +1,8 @@
 const fluid = require('./fluidOsc');
+const drumTrack909 = require('../recipes/drumTrack909');
 
+module.exports = fluid;
 module.exports.tab = require('./tab');
 module.exports.converters = require('./converters');
 module.exports.Client = require('./FluidClient');
-module.exports.recipes = {
-  drumTrack909: require('../recipes/drumTrack909'),
-};
-
-Object.assign(module.exports, fluid);
+module.exports.recipes = { drumTrack909 } ;
