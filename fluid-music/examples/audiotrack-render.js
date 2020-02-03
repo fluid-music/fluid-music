ed682bc755e45f755c5b104131ef1ebc33eb802b
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-02-03 15:50:20 -0500

    Rendering stops playback

--- fluid-music/examples/audiotrack-render.js
+++ fluid-music/examples/audiotrack-render.js
@@ -3,19 +3,17 @@ const fluid = require('../src/index');
 const Client = require('../src/FluidClient');
 const tabEx = require('./tab-examples');
 
+// first run simple-setup.js
+// second use this to save
+
 const client = new Client(9999);
 
 client.send([
-  fluid.global.activate(path.join(__dirname, 'sessions/out.tracktionedit'), true),
   fluid.audiotrack.select('main'),
-  fluid.midiclip.create('p2', 1, 8, tabEx.beatNotes),
-  fluid.plugin.select('zebra2', 'VST'),
+  fluid.midiclip.create('beatNotes', 1, 8, tabEx.beatNotes),
   fluid.audiotrack.renderRegion('media/p3.wav', 1, 8),
 
   fluid.audiotrack.select('wet'),
-  fluid.plugin.select('zebra2', 'vst'),
-  fluid.plugin.select('eos', 'VST'),
   ...fluid.midiclip.create('no-tears', 8, 8, tabEx.noTearsVerseNotes),
-  fluid.clip.render('media/chords1.wav', 10),
-  fluid.global.save(),
+  fluid.clip.render('media/noTearsVerseNotes.wav', 10),
 ]);
