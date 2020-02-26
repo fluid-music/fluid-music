a360d282bee0869d972dcc5271a3713d371398a6
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-02-26 15:25:32 -0500

    Added IPC Functionality, examples work

---
+++ Source/temp_OSCInputStream.cpp
@@ -0,0 +1,8 @@
+//
+//  temp_OSCInputStream.cpp
+//  cybr - ConsoleApp
+//
+//  Created by Zhi Wei Gan on 2/24/20.
+//
+
+#include "temp_OSCInputStream.hpp"
---
+++ Source/temp_OSCInputStream.h
@@ -0,0 +1,8 @@
+//
+//  temp_OSCInputStream.hpp
+//  cybr - ConsoleApp
+//
+//  Created by Zhi Wei Gan on 2/24/20.
+//
+
+#include "../JuceLibraryCode/JuceHeader.h"
