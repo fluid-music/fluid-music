37bc8df5c70270ec8c3f267a7751fa83d37f5f8e
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-03-31 15:36:30 -0400

    WIP demo-edm

--- demo-edm/index.js
+++ demo-edm/index.js
@@ -7,18 +7,17 @@ const wd           = process.cwd();
 const editFilename = path.join(wd, 'edm.tracktionedit');
 const bassFilename = path.join(__dirname, 'zebra2.bass2osc.trkpreset');
 const kickPath     = recipes.fromMars909.kit.k2.path
+
 console.log('kick path:', kickPath);
 console.log('session:', editFilename);
 
 const msg = [
   fluid.global.activate(editFilename, true),
-  // fluid.audiotrack.select('kick hi ho it is off to work we go hahahaha'),
-  // _.range(10).map((i) => fluid.audiotrack.insertWav('k'+i, i, kickPath)),
-  // fluid.audiotrack.select('bass'),
+  fluid.audiotrack.select('kick'),
+  _.range(10).map((i) => fluid.audiotrack.insertWav('k'+i, i, kickPath)),
+  fluid.audiotrack.select('bass'),
   fluid.plugin.loadTrkpreset(bassFilename),
 ];
 
-
-console.log(msg);
 const client = new fluid.Client(9999);
 client.send(msg);
