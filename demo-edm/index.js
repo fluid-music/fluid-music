5dbe4241a9bbc5680d69e3189c3c7b9bf7115a1f
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-03-31 14:07:28 -0400

    WIP demo-edm

--- demo-edm/index.js
+++ demo-edm/index.js
@@ -1,16 +1,24 @@
 const _ = require('underscore');
+const path = require('path');
 const fluid = require('fluid-music');
 const recipes = require('fluid-recipes');
 
-const kickPath = recipes.fromMars909.kit.k2.path
-console.log('kickpath', kickPath);
+const wd           = process.cwd();
+const editFilename = path.join(wd, 'edm.tracktionedit');
+const bassFilename = path.join(__dirname, 'zebra2.bass2osc.trkpreset');
+const kickPath     = recipes.fromMars909.kit.k2.path
+console.log('kick path:', kickPath);
+console.log('session:', editFilename);
 
 const msg = [
-  fluid.audiotrack.select('kick hi ho it is off to work we go hahahaha'),
-  _.range(10).map((i) => fluid.audiotrack.insertWav('k'+i, i, kickPath)),
+  fluid.global.activate(editFilename, true),
+  // fluid.audiotrack.select('kick hi ho it is off to work we go hahahaha'),
+  // _.range(10).map((i) => fluid.audiotrack.insertWav('k'+i, i, kickPath)),
+  // fluid.audiotrack.select('bass'),
+  fluid.plugin.loadTrkpreset(bassFilename),
 ];
 
 
 console.log(msg);
-const client = new fluid.UdpClient(9999);
+const client = new fluid.Client(9999);
 client.send(msg);
