06033421ceb0637cbff5506c82754bc8587ecf32
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-04-02 22:45:50 -0400

    update demo-edm

--- demo-edm/index.js
+++ demo-edm/index.js
@@ -1,6 +1,7 @@
 const fs      = require('fs');
 const path    = require('path');
 const _       = require('underscore');
+const R       = require('ramda');
 const YAML    = require('yaml');
 const fluid   = require('fluid-music');
 const recipes = require('fluid-recipes');
@@ -14,15 +15,31 @@ console.log('kick path:', kickPath);
 
 
 const bass = fluid.tab.parse(content);
+const octaveUp = R.map(n => {n.n += 36; return n});
 
 const msg = [
+  // Bass
   fluid.audiotrack.select('kick'),
   fluid.audiotrack.removeClips(),
   _.range(10).map((i) => fluid.audiotrack.insertWav('k'+i, i, kickPath)),
   fluid.audiotrack.select('bass'),
   fluid.midiclip.create('bass', 0, 8, bass),
-  fluid.transport.loop(0, 8),
+
+  // Mids
+  fluid.audiotrack.select('mallet'),
+  fluid.midiclip.create('mallet', 0, 8, octaveUp(bass)),
+  fluid.plugin.select('zebra2', 'vst'),
+  fluid.plugin.setParamNormalized('VCF1: Cutoff', 0.01),
+
+  // Delay
+  fluid.audiotrack.select('delayQuarterNote'),
+  fluid.pluginTStereoDelay.select(),
+  fluid.pluginTStereoDelay.setDelayMs(60000 / 128),
+
+  // Setup Looping
+  fluid.transport.loop(0, 7),
 ];
+
 
 const client = new fluid.Client(9999);
 client.send(msg);
--- demo-edm/template.js
+++ demo-edm/template.js
@@ -6,14 +6,30 @@ const editFilename = path.join(wd, 'edm.tracktionedit');
 const bassFilename = path.join(__dirname, 'zebra2.bass2osc.trkpreset');
 
 console.log('session:', editFilename);
+const bpm = 128;
+const oneBeatInMs = 60000 / 128;
+
+// map 0 to 1 over -50 to 18. Range of
+const db2DelayLevel = v => (v + 50) / 68;
+const ms2DelayTime = v => (v) / 5000;
 
 const msg = [
   fluid.global.activate(editFilename, true),
-  fluid.tempo.set(128),
+  fluid.tempo.set(bpm),
   fluid.audiotrack.select('kick'),
   fluid.audiotrack.select('bass'),
   fluid.plugin.loadTrkpreset(bassFilename),
+  fluid.audiotrack.select('mallet'),
+  fluid.plugin.select('zebra2', 'vst'),
+  fluid.plugin.setParamNormalized('Delay1: Mix', 0),
+  fluid.audiotrack.send('delayQuarterNote', -6),
+  // create delays
+  fluid.audiotrack.selectReturnTrack('delayQuarterNote'),
+  fluid.pluginTStereoDelay.select(),
+  fluid.pluginTStereoDelay.zero(),
+  fluid.pluginTStereoDelay.setDelayMs(oneBeatInMs),
 ];
+
 
 const client = new fluid.Client(9999);
 client.send(msg);
