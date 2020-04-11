3f8be19babbc05067b99b36a7bd44fa3debcd7d7
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-04-10 22:40:28 -0400

    parseScore outputs a tracksObject

--- fluid-music/src/tab.js
+++ fluid-music/src/tab.js
@@ -384,6 +384,9 @@ const parse = function(object, rhythm, noteLibrary, startTime, vPattern, vLibrar
  * input format, and outputs a more structured document that resembles the track
  * and clip model that most DAWs use.
  *
+ * In the input - arrays indicate a sequence
+ * In the output - arrays are a collection of clips (with .duration/.startTime)
+ *
  * @param {Object|Array|String} object - Typically this is the only argument you
  *    need. This is a score object that represents multiple tracks.
  *
@@ -395,11 +398,12 @@ const parse = function(object, rhythm, noteLibrary, startTime, vPattern, vLibrar
  * @param {number} [startTime]
  * @param {string} [vPattern]
  * @param {Object|Array} [vLibrary]
- * @param {Object} parentObject Used for recursion only. Internally, this object
- *    is the target on which new clips will be added.
+ * @param {Object} [tracksObject] This is a container for all tracks. It is
+ *    returned when `object` is a JS Object (as opposed to a string or array).
+ * @param {string} [trackKey] The name of the track that we are currently parsing.
  * @returns {Object} representation of the score
  */
-const parseScore = function(object, rhythm, noteLibrary, startTime, vPattern, vLibrary, parentObject) {
+const parseScore = function(object, rhythm, noteLibrary, startTime, vPattern, vLibrary, tracksObject, trackKey) {
   if (typeof startTime !== 'number') startTime = 0;
   if (object.hasOwnProperty('noteLibrary')) noteLibrary = object.noteLibrary;
   if (object.hasOwnProperty('vLibrary')) vLibrary = object.vLibrary;
@@ -416,27 +420,24 @@ const parseScore = function(object, rhythm, noteLibrary, startTime, vPattern, vL
   //
   // The array and object handlers must:
   // - call parseScore on children
-  // - pass the appropriate parentObject to all children's parseScore call
+  // - pass the appropriate tracksObject to all children's parseScore call
   //
   // The string handler must:
   // - create clips with a .startTime
-  // - add those clips to the parentObject.clips array
+  // - add those clips to the tracksObject.clips array
   //
   // The object handler:
   // - Must return a fully parsed representation of the score
   if (Array.isArray(object)) {
     let duration = 0;
+    const results = [];
     for (let o of object) {
-      let parent = (Array.isArray(o) || typeof o === 'string') ? parentObject : {
-        duration: 0
-      }
-      let result = parseScore(o, rhythm, noteLibrary, startTime + duration, vPattern, vLibrary, parentObject);
+      let result = parseScore(o, rhythm, noteLibrary, startTime + duration, vPattern, vLibrary, tracksObject, trackKey);
+      results.push(result);
       duration += result.duration;
     }
-    // If we want to add the duration of each segment, this is how:
-    // if (duration > object.duration) object.duration = duration;
-    // However, for now I'm avoiding modifying the input object.
-    return {duration};
+    results.duration = duration;
+    return results
   } else if (typeof object === 'string') {
     // We have a string that can be parsed with parseTab
     const a = parseRhythm(rhythm);
@@ -445,30 +446,30 @@ const parseScore = function(object, rhythm, noteLibrary, startTime, vPattern, vL
     result.startTime = startTime;
     result.duration = duration;
 
-    if (!parentObject.clips) parentObject.clips = [];
-    parentObject.clips.push(result);
+    if (!tracksObject[trackKey]) tracksObject[trackKey] = {clips:[]};
+    tracksObject[trackKey].clips.push(result);
 
     return result;
   } else {
     // Assume we have a JavaScript Object
-    if (parentObject === undefined) parentObject = { duration: 0, startTime };
+    const isOutermost = (tracksObject === undefined);
+    if (isOutermost) tracksObject = { };
 
+    let duration = 0;
     for (let [key, val] of Object.entries(object)) {
       if (reservedKeys.hasOwnProperty(key)) continue;
 
-      // create a new parent, or use an existing one
-      let newParent;
-      if (parentObject.hasOwnProperty(key)) {
-        newParent = parentObject[key];
-      } else {
-        newParent = { originalValue: val, key, startTime, duration: 0 };
-        parentObject[key] = newParent;
+      // Ensure track and track.clips exists
+      if (!tracksObject.hasOwnProperty(key)) {
+        tracksObject[key] = { clips: [] };
       }
 
-      let result = parseScore(val, rhythm, noteLibrary, startTime, vPattern, vLibrary, newParent);
-      if (result.duration > parentObject.duration) parentObject.duration = result.duration;
+      let result = parseScore(val, rhythm, noteLibrary, startTime, vPattern, vLibrary, tracksObject, key);
+      if (result.duration > duration) duration = result.duration;
     }
-    return parentObject;
+
+    tracksObject.duration = duration;
+    return tracksObject;
   }
 };
 
--- fluid-music/test/test-parse-score.js
+++ fluid-music/test/test-parse-score.js
@@ -48,12 +48,14 @@ describe('parseScore', () => {
       '.1..',
     ]};
     const result1 = tab.parseScore(s1);
+
     const clip1 = [{n: 0, s: 0.25, l: 0.25}];
-    const clip2 = [{n: 1, s: 0.25, l: 0.25}];
-    
     clip1.startTime = 0;
+    clip1.duration  = 1;
+
+    const clip2 = [{n: 1, s: 0.25, l: 0.25}];
     clip2.startTime = 1;
-    clip1.duration  = clip2.duration = 1;
+    clip2.duration  = 1;
     
     result1.should.containDeep({
       drums: {
@@ -62,7 +64,6 @@ describe('parseScore', () => {
     });
   });
 
-  it('should handle nested objects');
   it('should handle nested arrays', () => {
     const s1 = { noteLibrary, r, drums: [
       ['0...', '1...'],
@@ -75,7 +76,6 @@ describe('parseScore', () => {
 
     const result1 = tab.parseScore(s1);
     result1.should.containDeep({
-      duration: 4,
       drums: {
         clips: [ clip0, clip1, clip2, clip3 ],
       }
@@ -96,13 +96,76 @@ describe('parseScore', () => {
 
     const result1 = tab.parseScore(s1);
     result1.should.containDeep({
-      duration: 4,
       drums: {
         clips: [ clip0, clip2, clip3 ],
-        k: {
-          clips: [ clip1 ]
-        }
-      }
+      },
+      k: {
+        clips: [ clip1 ]
+      },
     });
+  });
+
+  it('should handle objects inside arrays', () => {
+    const s1 = { noteLibrary, r, main: [
+      '0...',
+      { drum: '1...'},
+      { drum: '2...'},
+      '3...'
+    ]};
+
+    // expected
+    const clip0 = [{n: 0, s: 0, l: 0.25}]; clip0.startTime = 0;
+    const clip1 = [{n: 1, s: 0, l: 0.25}]; clip1.startTime = 1;
+    const clip2 = [{n: 2, s: 0, l: 0.25}]; clip2.startTime = 2;
+    const clip3 = [{n: 3, s: 0, l: 0.25}]; clip3.startTime = 3;
+    const expectedResult = {
+      main: {
+        clips: [clip0, clip3],
+      },
+      drum: {
+        clips: [clip1, clip2],
+      },
+    };
+
+    const result1 = tab.parseScore(s1);
+    result1.should.containDeep(expectedResult);
+  });
+
+  it('should handle deeply nested objects', () => {
+    const s1 = { noteLibrary, r, main: [
+      {
+        drums: '0...',
+        other: {
+          r: '12341234',
+          p: '1'
+        },
+        d2: '2'
+      },
+      { drums: '3...'},
+      '4...'
+    ]};
+    const clip0 = [{n: 0, s: 0, l: 0.25}]; clip0.startTime = 0; clip0.duration = 1;
+    const clip1 = [{n: 1, s: 0, l: 0.25}]; clip1.startTime = 0; clip1.duration = 2;
+    const clip2 = [{n: 2, s: 0, l: 0.25}]; clip2.startTime = 0; clip2.duration = 1;
+    const clip3 = [{n: 3, s: 0, l: 0.25}]; clip3.startTime = 2; clip3.duration = 1;
+    const clip4 = [{n: 4, s: 0, l: 0.25}]; clip4.startTime = 3; clip4.duration = 1;
+
+    const expectedResult = {
+      main: {
+        clips: [clip4],
+      },
+      drums: {
+        clips: [clip0, clip3],
+      },
+      p: {
+        clips: [clip1]
+      },
+      d2: {
+        clips: [clip2]
+      },
+    };
+
+    const result1 = tab.parseScore(s1);
+    result1.should.containDeep(expectedResult);
   });
 });
