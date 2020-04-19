a9590c11242ca7562989ddfa8ddf88c81e4673f7
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-04-19 13:33:46 -0400

    Checked responses

--- fluid-music/examples/ipc-reply.js
+++ fluid-music/examples/ipc-reply.js
@@ -5,11 +5,16 @@ const client = new Client(9999);
 
 
 const m1 = [
-  fluid.audiotrack.select('hi'),
-  fluid.plugin.select('delay'),
+  fluid.audiotrack.select('track'),
+  fluid.midiclip.select('clip', 0, 10),
+  fluid.midiclip.note(10, 0, 0.25, 100),
+  fluid.midiclip.clear(),
+  fluid.clip.select('clip'),
 ]
 
 client.send(m1)
 .then((data) => {
-    console.log(data)
+    for(elem of data.elements){
+      console.log(elem);
+    }
 });
