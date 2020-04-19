250c7e29d47a667206ad5df025dd04b3e1e43997
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-04-19 13:45:22 -0400

    quick fix

--- fluid-music/examples/ipc-reply.js
+++ fluid-music/examples/ipc-reply.js
@@ -5,16 +5,11 @@ const client = new Client(9999);
 
 
 const m1 = [
-  fluid.audiotrack.select('track'),
-  fluid.midiclip.select('clip', 0, 10),
-  fluid.midiclip.note(10, 0, 0.25, 100),
-  fluid.midiclip.clear(),
-  fluid.clip.select('clip'),
+  fluid.audiotrack.select('hi'),
+  fluid.plugin.select('delay'),
 ]
 
 client.send(m1)
 .then((data) => {
-    for(elem of data.elements){
-      console.log(elem);
-    }
+    console.log(data)
 });
