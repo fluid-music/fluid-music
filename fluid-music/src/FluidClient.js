60fe94d606984a195d5ce2d2f79dd25f3bb813a0
Zhi Wei Gan <zhiwei_gan@hotmail.com>
2020-04-26 16:04:51 -0400

    Fixed promise rejection errors

--- fluid-music/src/FluidClient.js
+++ fluid-music/src/FluidClient.js
@@ -17,34 +17,45 @@ module.exports = class FluidClient{
     this.client = new IpcClient(ipcOptions);
   }
 
-  _connectPromise(){
-    return new Promise((resolve, reject) => {
-      this.client.once('connect', () => {
-        resolve('connected');
-      });
-
-      // IpcClient emits an error when the port is not listening. How else can
-      // it fail?
-      this.client.once('error', (error) => {
-        reject(error);
-      });
-    });
-  }
-
-  _replyPromise(){
-    return new Promise((resolve, reject) => {
-      this.client.on("res", (data, address)=>{
-        // console.log(osc.fromBuffer(data), 'from', address);
-        resolve(osc.fromBuffer(data))
-        this.client.close();
-      })
-    })
-  }
-
   async send(msgObject, timetag){
-    await this._connectPromise();
-    await this.client.sendOsc(msgObject, timetag);
-    return this._replyPromise();
+    
+    const connectPromise = () => {
+        return new Promise((resolve, reject) => {
+          this.client.once('connect', () => {
+            resolve('connected');
+          });
+    
+          this.client.once('error', (error) => {reject(error)});
+          this.client.once('close', (error) => {reject(error)});
+          this.client.once('timeout', () => {reject("Connection Timed Out")});
+        });
+    }
+
+    const replyPromise = () => {
+        return new Promise((resolve, reject) => {
+          this.client.on("res", (data)=>{
+            try{
+                resolve(osc.fromBuffer(data))
+            }
+            catch (err){
+                console.log(err);
+            }
+            this.client.close();
+          })
+    
+          this.client.once('error', (error) => {reject(error)});
+          this.client.once('close', (error) => {reject(error)});
+          this.client.once('timeout', () => {reject("Connection Timed Out")});
+        })
+    }
+    try{
+        await connectPromise();
+        await this.client.sendOsc(msgObject, timetag);
+        return replyPromise();
+    }
+    catch (err){
+        console.log(err);
+    }
   }
 
   get targetPort() {
