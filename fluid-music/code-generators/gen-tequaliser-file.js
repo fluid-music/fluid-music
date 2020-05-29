e2f0b75ab209c5da38f9bef8451b4736c49b7e70
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-05-28 23:52:17 -0400

    remove some misleading code generators.
    The presense of these suggests that they can still be used. They cannot. The files they generated
    have been manually edited to much after their generation

--- fluid-music/code-generators/gen-tequaliser-file.js
+++
@@ -1,8 +0,0 @@
-const path  = require('path');
-const fs    = require('fs');
-const tools = require('./tools');
-
-let paramNames = require('./data/tequaliser.js');
-let stream     = fs.createWriteStream(path.join(__dirname, '..', 'src/fluid/plugin-tequaliser.js'));
-
-tools.writePluginHelperFile('#TEqualiser', paramNames, stream);
--- fluid-music/code-generators/gen-treverber8-file.js
+++
@@ -1,8 +0,0 @@
-const path  = require('path');
-const fs    = require('fs');
-const tools = require('./tools');
-
-let paramNames = require('./data/treverber8.js');
-let stream     = fs.createWriteStream(path.join(__dirname, '..', 'src/fluid/plugin-treverber8.js'));
-
-tools.writePluginHelperFile('#TReverber8', paramNames, stream);
