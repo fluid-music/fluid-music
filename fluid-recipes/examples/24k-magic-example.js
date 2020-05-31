d38d0c1c7199b672dde729bd1a5fb08635678e87
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-05-31 13:53:57 -0400

    recipes: improve 24k magic example

--- fluid-recipes/examples/24k-magic-example.js
+++ fluid-recipes/examples/24k-magic-example.js
@@ -12,8 +12,23 @@ const session = fluid.score.parse(chorus);
 const client = new fluid.Client();
 client.send([
   fluid.global.activate(sessionPath, true),
+  fluid.audiotrack.selectReturnTrack('short reverb'),
+  recipes.presets.tReverber8.shortReverb(),
+
   fluid.audiotrack.select('synth'),
-  fluid.plugin.select('zebra2'),
+  fluid.pluginZebra2Vst2.select(),
+  fluid.pluginZebra2Vst2.setDelay1Mix(0),
+  fluid.audiotrack.send('short reverb', -13),
+
+  fluid.audiotrack.select('kick'),
+  fluid.pluginTEqualiser.select(),
+
+  // Tighten the kick drum by cutting some low mids...
+  fluid.pluginTEqualiser.setBand1(230, -4.4, 1.8),
+  // ..and boosting the highs with a shelf
+  fluid.pluginTEqualiser.setBand8(3000, 1.5, 1),
+  fluid.pluginTEqualiser.setBand8Shape(fluid.pluginTEqualiser.shape.highShelf),
+
   fluid.score.tracksToFluidMessage(session.tracks),
   fluid.transport.loop(0, session.duration),
   fluid.global.save(),
