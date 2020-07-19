46302197594f5b9fec98ea2f9fef3040479a65f4
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-07-19 15:19:37 -0400

    comments: describe current eventMapper behavior. closes #71

--- fluid-music/src/score.js
+++ fluid-music/src/score.js
@@ -4,7 +4,6 @@ const fluid   = require('./fluid/index');
 const mappers = require('./event-mappers');
 
 const parseTab     = tab.parseTab;
-const parseRhythm  = tab.parseRhythm;
 const reservedKeys = tab.reservedKeys;
 
 /**
@@ -135,6 +134,26 @@ function applyEventMappers(session, eventMappers=mappers.default) {
         data: {},
       };
 
+      // The code below shows what data are available inside eventMapper
+      // callbacks. The contents of context.clip.events can look different to
+      // each round of callbacks. Note the order of the loops: In the first
+      // round, the first callback will be called on each event, potentially
+      // removing that event from clip.events. Then, the second callback in the
+      // eventMappers array will be called on each event. During the second
+      // round of callbacks, clip.events may have a different length than during
+      // the first round. This will happen if, for example, the callbacks in the
+      // first round returned falsy values (removing events from clip.events in
+      // subsequent rounds) or arrays (adding events to clip.events). Note that
+      // changes to the contents of context.clip.events only take effect between
+      // rounds. The exception would be if a callback explicitly modifies
+      // context.clip.events -- however this would be a very poor design choice.
+      // Callbacks should never need to do this and it could cause subtle bugs.
+      //
+      // A callback that needs access to the original list of events can access
+      // it via the clip.unmappedEvents array. Note that even though events in
+      // the unmappedEvents array will not be added or removed by eventMappers,
+      // they will still be mutated by event mappers, so the event.n members
+      // will likely not look the same as the objects in the .nLibrary objects.
       clip.unmappedEvents = clip.events;
       let events = clip.events.flat();
       for (const eventMapper of eventMappers) {
