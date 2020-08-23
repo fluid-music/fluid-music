e57596579597e0727a7bb9721c3a7aa03782f5a9
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-08-23 19:10:17 -0400

    remove obsolete examples

--- fluid-music/examples/obsolete/read-yaml-example.js
+++
@@ -1,8 +0,0 @@
-const util = require('util');
-const fs  = require('fs');
-const YAML = require('yaml');
-
-const file = fs.readFileSync('basics.yaml', 'utf8');
-const result = YAML.parse(file);
-
-console.log(util.inspect(result, {showHidden: false, depth: null, colors: true}));
