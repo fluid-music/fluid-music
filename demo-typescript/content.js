f2efd18ea0870bf243ea6e323380a4c0b96134c5
Charles Holbrow <CharlesHolbrow@gmail.com>
2020-10-21 12:32:47 -0400

    remove junk test automation from demo-typescript. closes #95

--- demo-typescript/content.js
+++ demo-typescript/content.js
@@ -30,22 +30,17 @@ eq.setBand2(330, -3)
 ////////////////////////////////////////////////////////////////
 // Create score events (automation/midi/samples/etc)
 
-// Automation Point
-const f = fluid.PodolskiVst2.makeAutomation.vcf0Cutoff(0.5)
-const p = fluid.DragonflyRoom.makeAutomation.sizeMeters(9)
-const q = fluid.DragonflyRoom.makeAutomation.sizeMeters(30)
+const automationLibrary = {
+  a: new fluid.techniques.TrackAuto({ paramKey: 'pan', value: -.5, curve: -0.5 }),
+  b: new fluid.techniques.TrackAuto({ paramKey: 'pan', value:  .5 }),
+  c: new fluid.techniques.TrackAuto({ paramKey: 'gain', value: -6, curve: 0.8 }),
+  d: new fluid.techniques.TrackAuto({ paramKey: 'gain', value: -32.9 }),
+  e: new fluid.techniques.TrackAuto({ paramKey: 'width', value: 0 }),
+  f: new fluid.techniques.TrackAuto({ paramKey: 'width', value: 1 }),
 
-const r = new fluid.techniques.TrackAuto({ paramKey: 'pan', value: -.5, curve: -0.5 })
-const m = new fluid.techniques.TrackAuto({ paramKey: 'pan', value:  .5 })
-const n = new fluid.techniques.TrackAuto({ paramKey: 'gain', value: -6, curve: 0.8 })
-const u = new fluid.techniques.TrackAuto({ paramKey: 'gain', value: -32.9 })
-const x = new fluid.techniques.TrackAuto({ paramKey: 'width', value: 0 })
-const y = new fluid.techniques.TrackAuto({ paramKey: 'width', value: 1 })
-
-const tLibrary = {
-  f, p, q,
-  r, m, n, u, x, y,
-  ...drums.tLibrary
+  u:fluid.PodolskiVst2.makeAutomation.vcf0Cutoff(0.5),
+  v:fluid.DragonflyRoom.makeAutomation.sizeMeters(9),
+  w:fluid.DragonflyRoom.makeAutomation.sizeMeters(30),
 }
 
 const chordLibrary = fluid.tLibrary.fromArray(chords.map(chord => new fluid.techniques.MidiChord(chord)))
@@ -66,16 +61,18 @@ const dLibrary = {
 let session = new fluid.FluidSession({
   bpm: 96,
   r: '1 + 2 + 3 + 4 + ',
-  dLibrary, // default for kick and snare
-  tLibrary, // default for kick and snare
+  // default for kick and snare
+  tLibrary: drums.tLibrary,
+  // default for kick and snare
+  dLibrary,
 }, {
-  kick:  { d: '.   . mf      ', gain: -6, plugins: [comp, eq]},
+  kick:  { d: '.   . mf      ', gain: -6, plugins: [comp, eq] },
   snare: { d: 'm   f   m   f ' },
   chrd:  { tLibrary: chordLibrary, pan: -.25, plugins: [pwmSynth], gain: -10 },
   bass:  { tLibrary: bassLibrary, plugins: [bassSynth] },
   tamb:  { pan: .25 },
-  revb:  { plugins: [verbPlugin], tLibrary: { p, q, r, m, n, u, x, y } },
-});
+  revb:  { plugins: [verbPlugin], tLibrary: automationLibrary },
+})
 
 session.insertScore({
   kick: ['.   . dd-dD--D--', 'd-- d-- d-- d-- '],
@@ -83,7 +80,8 @@ session.insertScore({
   tamb: ['t t t t t t t t ', {r: '1....234..', tamb: 'Ttttt..ttt', d: 'p'} ],
   bass:  '        b-      ',
   chrd:  'a-  .  ab---    ',
-  revb:  'p      qx  ynrmu',
+  //     '1 + 2 + 3 + 4 + ',
+  revb:  'e       f       ',
 });
 
 // const templateMessage = fluid.sessionToTemplateFluidMessage(session);
