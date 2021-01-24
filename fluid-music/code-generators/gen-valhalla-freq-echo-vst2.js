874ed59099b60ee3a9bef783ea5376222ca01908
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-01-23 19:28:03 -0500

    Add VST adapter: ValhallaFreqDelay VST2

---
+++ fluid-music/code-generators/gen-valhalla-freq-echo-vst2.js
@@ -0,0 +1,58 @@
+const freqEchoReport = require('./reports/valhalla-freq-echo-vst2')
+const fluid = require('..')
+
+for (const paramInfo of freqEchoReport.params) {
+  if (paramInfo.name === 'lowCut') {
+    paramInfo.isLinear = true
+    paramInfo.range = [20, 3000]
+  }
+  if (paramInfo.name === 'highCut') {
+    paramInfo.isLinear = true
+    paramInfo.range = [50, 15000]
+  }
+  if (paramInfo.name === 'delay') {
+    // Used this graph to get close
+    // https://www.desmos.com/calculator/eltrko6ete
+    // 
+    // Then fine tune manually with .range = [0.01, 1000]
+    // 3.321 is low
+    // 3.322 is exact@100ms, low@300ms
+    // 2.3225 is high@100ms and 300ms
+    // 3.3222 is high
+    paramInfo.powerFuncB = 3.3221
+  }
+  if (paramInfo.name === 'sync') {
+    // Note that the choices array below is a little unusual. Usually we would
+    // leave the .range at [0, 1]. In this case the guesser correctly identifies
+    // that this parameter is linear from 0-23. To keep things simple we'll leave
+    // that range intact.
+    paramInfo.choices = {
+      'free':  1,
+      '1/64':  2,
+      '1/32':  3,
+      '1/16':  4,
+      '1/8':   5,
+      '1/4':   6,
+      '2/4':   7,
+      '4/4':   8,
+      '8/4':   9,
+      '1/64.': 10,
+      '1/32.': 11,
+      '1/16.': 12,
+      '1/8.':  13,
+      '1/4.':  14,
+      '2/4.':  15,
+      '4/4.':  16,
+      '1/64T': 17,
+      '1/32T': 18,
+      '1/16T': 19,
+      '1/8T':  20,
+      '1/4T':  21,
+      '2/4T':  22,
+      '4/4T':  23
+    }
+  }
+}
+
+const moduleString = fluid.gen.generatePluginModule(freqEchoReport)
+process.stdout.write(moduleString)
---
+++ fluid-music/src/plugin-adapters/valhalla-freq-echo-vst2.ts
@@ -0,0 +1,110 @@
+import { PluginType, FluidPlugin } from '../FluidPlugin';
+import { PluginAutomation } from '../techniques';
+const pluginName = 'ValhallaFreqEcho'
+const pluginType = PluginType.VST2
+
+export interface ValhallaFreqEchoVst2Parameters {
+  /** percent value from 0 to 100 */
+  wetDryPercent? : number;
+  /** hz value from -1000 to 1000 */
+  shiftHz? : number;
+  /** ms value from 0.01 to 1000 */
+  delayMs? : number;
+  /** value from 0 to 23 */
+  sync? : number;
+  /** percent value from 0 to 100 */
+  feedbackPercent? : number;
+  /** hz value from 20 to 3000 */
+  lowCutHz? : number;
+  /** hz value from 50 to 15000 */
+  highCutHz? : number;
+  stereo? : number;
+}
+const parameterLibrary = {
+  wetDryPercent: { name: 'wetDry', index: 0, isLinear: true, range: [0, 100] as [number, number], units: 'percent' },
+  shiftHz: { name: 'shift', index: 1, isLinear: true, range: [-1000, 1000] as [number, number], units: 'hz' },
+  delayMs: { name: 'delay', index: 2, isLinear: false, range: [0.01, 1000] as [number, number], units: 'ms', powerFuncB: 3.3221 },
+  sync: { name: 'sync', index: 3, isLinear: true, range: [0, 23] as [number, number], choices: {"free":1,"1/64":2,"1/32":3,"1/16":4,"1/8":5,"1/4":6,"2/4":7,"4/4":8,"8/4":9,"1/64.":10,"1/32.":11,"1/16.":12,"1/8.":13,"1/4.":14,"2/4.":15,"4/4.":16,"1/64T":17,"1/32T":18,"1/16T":19,"1/8T":20,"1/4T":21,"2/4T":22,"4/4T":23} },
+  feedbackPercent: { name: 'feedback', index: 4, isLinear: true, range: [0, 100] as [number, number], units: 'percent' },
+  lowCutHz: { name: 'lowCut', index: 5, isLinear: true, range: [20, 3000] as [number, number], units: 'hz' },
+  highCutHz: { name: 'highCut', index: 6, isLinear: true, range: [50, 15000] as [number, number], units: 'hz' },
+  stereo: { name: 'stereo', index: 7, isLinear: false, choices: {"mono":0,"stereo":1} }
+}
+const makeAutomation = {
+  wetDryPercent (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'wetDryPercent',
+    });
+  },
+  shiftHz (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'shiftHz',
+    });
+  },
+  delayMs (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'delayMs',
+    });
+  },
+  sync (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'sync',
+    });
+  },
+  feedbackPercent (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'feedbackPercent',
+    });
+  },
+  lowCutHz (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'lowCutHz',
+    });
+  },
+  highCutHz (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'highCutHz',
+    });
+  },
+  stereo (value? : number, curve = 0) {
+    return new PluginAutomation({
+      value,
+      curve,
+      pluginSelector: { pluginName, pluginType },
+      paramKey: 'stereo',
+    });
+  }
+}
+export class ValhallaFreqEchoVst2 extends FluidPlugin {
+  constructor(
+    public readonly parameters : ValhallaFreqEchoVst2Parameters = {},
+  ) { super(pluginName, pluginType) }
+
+  readonly parameterLibrary = parameterLibrary;
+  readonly makeAutomation = makeAutomation;
+
+  // Static members
+  static readonly parameterLibrary = parameterLibrary;
+  static readonly makeAutomation = makeAutomation;
+}
