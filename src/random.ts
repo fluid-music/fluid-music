beb14cc7994418a47c699c6b0aa3ddfa06861339
Charles Holbrow <CharlesHolbrow@gmail.com>
2021-04-17 15:09:49 -0400

    random.shuffle

--- src/random.ts
+++ src/random.ts
@@ -10,3 +10,22 @@ export function choice(input) {
   else
     return input[choice(Object.keys(input))];
 }
+
+export function shuffle(array : any[]) {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
